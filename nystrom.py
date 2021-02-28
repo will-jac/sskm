ac4a9a7b01e266dcbfd242ad960158676a6478dc
Jack Williams <j.william@ou.edu>
2021-02-28 11:27:35 -0600

    added random features

---
+++ nystrom.py
@@ -0,0 +1,131 @@
+import numpy as np
+
+from kernel import RidgeKernel
+from sklearn.metrics.pairwise import pairwise_kernels
+
+from scipy.linalg import svd
+
+rng = np.random.default_rng()    
+
+class NystromTransformer():
+    # https://www.stat.berkeley.edu/~mmahoney/pubs/kernel_JMLR.pdf
+
+    def __init__(self, kernel, n_components = 100, gamma=None, degree=None, coef0=None, 
+            kernel_params=None, n_jobs=None):
+        self.kernel = kernel
+        self.n_components = n_components
+        self.gamma = gamma
+        self.degree = degree
+        self.coef0 = coef0
+        self.kernel_params = kernel_params
+        self.n_jobs = n_jobs
+
+    def _compute_kernel(self, X, K=None):
+        if callable(self.kernel):
+            params = self.kernel_params
+        else:
+            params = {"gamma": self.gamma, "degree": self.degree, "coef0": self.coef0}
+        return pairwise_kernels(X, K, metric=self.kernel, filter_params=True, 
+                n_jobs=self.n_jobs, **params)
+
+    def fit3(self, X, y):
+        # make sure that the shapes will work
+        if X.shape[0] < self.n_components:
+            print('warning: more components than samples, rescaling to number of samples')
+            n_components = X.shape[0]
+        else:
+            n_components = self.n_components
+
+        # TODO: probability sampling
+        samp = rng.permutation(X.shape[0])
+        samp = samp[0:n_components]
+        # X basis 
+        X_hat = X[samp]
+
+        A = self._compute_kernel(X, X_hat)
+        B = A[samp, samp]   
+
+    def fit2(self, X, y):
+        # https://www.stat.berkeley.edu/~mmahoney/pubs/kernel_JMLR.pdf
+        
+        # make sure that the shapes will work
+        if X.shape[0] < self.n_components:
+            print('warning: more components than samples, rescaling to number of samples')
+            n_components = X.shape[0]
+        else:
+            n_components = self.n_components
+
+        # TODO: probability sampling
+        samp = rng.permutation(X.shape[0])
+        samp = samp[0:n_components]
+        # X basis 
+        self.X_hat = X[samp]
+
+        self.C = []
+        for xhat in self.X_hat:
+            c = []
+            for x in X:
+                c.append(self.kernel(x, xhat))
+            self.C.append(c)
+        self.C = np.array(self.C)
+        print('shape of C:', self.C.shape)
+        
+        print('alternative method:', self._compute_kernel(X, self.X_hat).shape)
+
+        # TODO: scaling
+
+    def fit(self, X, y):
+        # make sure that the shapes will work
+        if X.shape[0] < self.n_components:
+            print('warning: more components than samples, rescaling to number of samples')
+            n_components = X.shape[0]
+        else:
+            n_components = self.n_components
+        # first, sample columns of the kernel matrix K
+
+        # sample m rows of the data
+        samp = rng.permutation(X.shape[0])
+        samp = samp[0:n_components]
+        # X basis 
+        self.X_hat = X[samp]
+
+        # compute the kernel matrix for our basis kernel
+        K_hat = self._compute_kernel(self.X_hat)
+        print('computed K_hat with shape', K_hat.shape)
+
+        # K_b = self._compute_kernel(X, self.X_hat)
+        # K_r = K_b @ np.linalg.inv(K_hat) @ K_b.T
+
+        # print('computing svd of K_r', K_r.shape)
+        U, S, V = svd(K_hat)
+
+        S = np.maximum(S, 1e-12)
+        self.normalization_ = np.dot(U / np.sqrt(S), V)
+        self.components_ = self.X_hat
+        self.component_indices_ = samp
+        print('normalization has shape', self.normalization_.shape)
+        print('components has shape', self.X_hat.shape)
+        
+        # 1 / D creates the inverse, only true because it's diagonal
+        # d = d[0:n_components]
+        # D_inverse = np.diag(np.sqrt(1 / np.maximum(d, 1e-12)))
+
+        # VtL = Vt[0:n_components,:]
+
+        # # print(D_inverse)
+        # print('computing Z from D, V', D_inverse.shape, VtL.shape)
+        # self.Z = D_inverse @ VtL
+        # print('Z has shape', self.Z.shape)
+        return self
+    
+    def transform(self, X):
+        print('transforming X with shape', X.shape)
+        # kernel_space_X = self._compute_kernel(X, self.X_hat)
+        kernel_space_X = self._compute_kernel(X, self.X_hat)
+        print('X in the kernel space has shape', kernel_space_X.shape)
+        to_ret = np.dot(kernel_space_X, self.normalization_.T)
+        print('normalized X has shape', to_ret.shape)
+        return to_ret
+        # z = self.Z.T @ kernel_space_X.T
+        # print('z(X) has shape', z.shape)
+        # return z.T
---
+++ random_fourier_features.py
@@ -0,0 +1,122 @@
+# Random Features for Large-Scale Kernel Machines
+# https://people.eecs.berkeley.edu/~brecht/papers/07.rah.rec.nips.pdf
+
+# Will be implementing the Random Fourier Features described above
+
+# algorithm:
+# Compute the fourier transform p of the kernel k: p(w) = 1/(2pi) \int e^{-jw'\delta}k(\delta) d\Delta
+# Draw D iid samples w1 ... wD in Rd from p and D iid samples b1 .. bD in R from [0, 2pi]
+# z(x) = \sqrt(2/D) [cos(w1' x b1) ... cos(wD' x + bD)]'
+
+import numpy as np
+from numpy import random
+
+# standard least-squares regression
+from sklearn.linear_model import Ridge
+# allows co-variance computation
+from scipy.linalg import cholesky, cho_solve
+
+# transformations possible
+
+def gaussian(w, D):
+    return (2*np.pi)**(-D/2) * np.exp(np.linalg.norm(w)**2 / (-2))
+
+def laplacian(w, D):
+    transf = lambda w : 1/(np.pi*(1+w**2))
+    return np.prod(np.apply_along_axis(transf, axis=0, arr=w))
+
+def cauchy(w, D):
+    return -1 # TODO
+
+rng = np.random.default_rng()    
+
+class rff:
+
+    def __init__(self, D = 10, sigma = 1.0, alpha=1.0, k = 'gaussian', method = 'linear'):
+        """Random Fourier Featurues
+        D : Dimension of random features
+        k : kernel to use (`gaussian`, `laplacian`)
+        method : method used to solve linear regression (`linear`, `cholesky`)
+        """
+        if k == 'laplacian':
+            self.p = laplacian
+        elif k == 'cauchy':
+            self.p = cauchy
+        else:
+            self.p = gaussian # default
+
+        if method == 'cholesky':
+            self._fit = self._fit_cholesky
+        else:
+            self._fit = self._fit_linear
+            self.lm = Ridge(alpha=alpha)
+
+        self.sigma = sigma
+        self.D = D
+        self.b = None
+        self.W = None
+
+    def _fit_linear(self, y):
+        self.lm.fit(self.Z.T, y)
+        
+    def _fit_cholesky(self, y):
+
+        sigma_I = self.sigma * np.eye(self.N)
+        self.K = self.Z.T @ self.Z + sigma_I
+
+    def fit(self, X, y):
+        """Fit model with data X and labels y
+        """
+        # compute W, b
+        # pull W from the normal distribution, and b from 0->2 pi
+
+        self.N, d = X.shape
+
+        # weights go from R^d -> R^D
+        self.W = rng.normal(loc=0, scale=1, size=(self.D, d))
+        # bias is in R, need D terms
+        self.b = rng.uniform(0, 2*np.pi, size=self.D)
+
+        self.Z = self.compute_features(X)
+        
+        self._fit(y)
+        self.fitted = True
+
+        # now solve the least-squares problem:
+        # min_w ||Z'w - y||_2^2 + \lambda ||w||_2^2
+
+        # done via linear equation solver, eg:
+        # A x = b to solve x
+        # use cholesky solver: https://docs.scipy.org/doc/scipy/reference/generated/scipy.linalg.cho_solve.html
+
+        # self.L = cholesky(self.kernel, lower=True)
+        # self.alpha = cho_solve((self.L, True), y)
+
+    def predict(self, X):
+        # if self.alpha is None:
+        #     print('error: model not fitted')
+        #     return
+        
+        # Z = self.compute_features(X)
+        # K = Z.T @ self.Z
+        # y_mean = K.dot(self.alpha)
+
+        # v = cho_solve((self.L, True), K.T)
+        # y_cov = (Z.T @ Z) - K.dot(v)
+
+        # return y_mean, y_cov
+
+        Z = self.compute_features(X)
+        return self.lm.predict(Z.T)
+
+    def compute_features(self, X):
+        # TODO: tweak as described https://teddykoker.com/2020/11/performers/
+        N, d = X.shape
+            
+        B = np.repeat(self.b[:, np.newaxis], N, axis=1)
+
+        # @ = matix multiplication
+        # print('W:', self.W.shape, 'X.T:', X.T.shape)
+        Z = 1.0/np.sqrt(2.0*self.D) * np.cos(self.sigma * self.W @ X.T + B)
+
+        return Z
